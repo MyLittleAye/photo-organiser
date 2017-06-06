4cfbd3e12edafd1e1d8564e51e94f55e5e5dc6b6
My Little Aye <mylittleaye@hotmail.com>
2017-06-06 22:49:41 +0100

    Initial project structure creation

---
+++ photo-org-model/src/main/java/com/jxt/App.java
@@ -0,0 +1,13 @@
+package com.jxt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ scripts/project-create.sh
@@ -0,0 +1,8 @@
+# 1. Create the project structure
+# ===============================
+# mvn archetype:generate -DgroupId=com.jxt -DartifactId=photo-org-model -DarchetypeArtifactId=maven-archetype-quickstart
+# mvn archetype:generate -DgroupId=com.jxt -DartifactId=photo-org-core -DarchetypeArtifactId=maven-archetype-quickstart
+# mvn archetype:generate -DgroupId=com.jxt -DartifactId=photo-org-rest -DarchetypeArtifactId=maven-archetype-webapp
+# mvn archetype:generate -DgroupId=com.jxt -DartifactId=photo-org-test -DarchetypeArtifactId=maven-archetype-quickstart
+
+
